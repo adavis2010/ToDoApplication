ace5c4e7794a037f503c1c44e1148176fa1ba66d
max-student <max-student@maxtrain.com>
2021-03-02 11:45:34 -0500

    commit

--- ToDoApplication/Cli.cs
+++ ToDoApplication/Cli.cs
@@ -4,19 +4,26 @@ using System.Text;
 
 namespace ToDoApplication {
     public class Cli {
+
+        public static DateTime? GetDateTime(string prompt) {
+            var response = GetString(prompt);
+            if(response.Length == 0) {
+                return null;
+            }
+                return Convert.ToDateTime(response);
+        }
+
         //method to return integer
         public static int GetInt (string prompt) {
             var response = GetString(prompt);
                 return Convert.ToInt32(response); 
-            
-
         }
 
         public static string GetString(string prompt) {
             Console.Write($"{prompt}: ");
             //takes no parameters
             var response = Console.ReadLine();
-            return response;
+                return response;
         }
 
 
--- ToDoApplication/Program.cs
+++ ToDoApplication/Program.cs
@@ -4,8 +4,16 @@ namespace ToDoApplication {
     class Program {
         static void Main(string[] args) {
             //calling our get string by using static method Cli.GetString
-            var answer = Cli.GetString("Enter a string");
-            Console.WriteLine($"The answer is {answer}");
+            var aStr = Cli.GetString("Enter a string");
+            var anInt = Cli.GetInt("Enter an int");
+            var aDate = Cli.GetDateTime("Enter a valid date");
+            var aDate2 = Cli.GetDateTime("Just press enter");
+
+            Console.WriteLine($"{aStr}");
+            Console.WriteLine($"{anInt}");
+            Console.WriteLine($"{aDate} [{aDate2}]");
+
+
 
 
         }
